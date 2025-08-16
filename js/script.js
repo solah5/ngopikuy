2a3c1347604bc4140414fb0356ba673476f985a8
solah5 <solahslh89@gmail.com>
2025-08-16 12:32:58 +0700

    membuat website kedai kopi ngopikuy

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    navbarNav.classList.toggle('active');
+    e.preventDefault(); // Mencegah aksi default link
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
